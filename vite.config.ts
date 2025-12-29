9033d1924550474968e9d7e65aed72edb5fa0440
Lavansai <bandarilavansai@gmail.com>
2025-12-29 12:09:20 +0530

    fix: set base path for github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Set base to your repository name
+  base: "/Min_Proj/", 
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
